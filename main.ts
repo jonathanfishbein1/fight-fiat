e52da11bfcbbacf75bef87a47eec3c06be069451
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2024-01-11 16:51:19 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -544,7 +544,7 @@ function initAliens () {
 }
 function buildSplashScreens () {
     splashScreens = []
-    headlines = [["Powered By Starcada"], []]
+    headlines = [["Powered By Starcada"]]
     currFont = drawStrings.createFontInfo(FontName.Font5)
     for (let value of headlines) {
         splashScreen = splashBase.clone()
