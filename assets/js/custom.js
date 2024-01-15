4f1fe902595cb0a4e0ca56520aca3d0386167c23
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2024-01-15 18:55:49 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,5 +14,5 @@ addSimMessageHandler("web", (data) => {
     const targetWindow = window.parent
     console.log('window ', window)
     console.log('targetWindow ', targetWindow)
-    targetWindow.postMessage('hello from iframe', '*')
+    targetWindow.postMessage(data, '*')
 })
